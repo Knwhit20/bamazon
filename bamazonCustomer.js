1ebdbfaa6e50c8258f6cd120de0643933f77d917
Kristen Stroup <kristenwhitson@gmail.com>
2019-10-07 21:19:18 -0500

    inquirer question prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,13 +11,40 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    readBamazon();
+    startBamazon();
+    purchase();
 })
 
-function readBamazon(){
+function startBamazon(){
     connection.query("SELECT id, product_name, price FROM products", function(err, res){
         if (err) throw err;
 
-        console.log(res);
+        // console.log(res);
+        for (var i = 0; i < res.length; i++){
+            console.log("Id: " + res[i].id +  " || Product: " + res[i].product_name + " || Price: " + res[i].price);
+           
+           
+        }
     })
 };
+
+
+function purchase(){
+    inquirer.prompt([
+        {
+        type: "list",
+        name: "rawList",
+        message: "What is the ID of the product you would like to buy?",
+        choices:[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        },
+        {
+        type: "input",
+        name: "numUnits",
+        message: "How many units would you like to purchase?",
+        
+        }
+    ]).then(function(){
+        console.log("congrats you purchased something!")
+    })
+
+}
