5ea19e97cc3b3e4f80c58a23f7d16f2a6af876f0
Kristen Stroup <kristenwhitson@gmail.com>
2019-10-09 16:36:20 -0500

    read me file, chosen product assigned from answer

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # bamazon
+
+Bamazon Node.js and MySQL application
+
+In this activity, you'll be creating an Amazon-like storefront with the MySQL skills you learned this unit. The app will take in orders from customers and deplete stock from the store's inventory. 
+
+Project Instructions
+1. Create a MySQL Database called `bamazon`.
+
+2. Then create a Table inside of that database called `products`.
+
+3. The products table should have each of the following columns:
+
+   * item_id (unique id for each product)
+
+   * product_name (Name of product)
+
+   * department_name
+
+   * price (cost to customer)
+
+   * stock_quantity (how much of the product is available in stores)
+
+4. Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
+
+5. Then create a Node application called `bamazonCustomer.js`. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+
+6. The app should then prompt users with two messages.
+
+   * The first should ask them the ID of the product they would like to buy.
+   * The second message should ask how many units of the product they would like to buy.
+
+7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+   * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+
+8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+   * This means updating the SQL database to reflect the remaining quantity.
+   * Once the update goes through, show the customer the total cost of their purchase.
+
+
+Technologies Used:
+Node.Js
+MySQL Workbench
+
+Connect Node.js with MySQL database
+![Image of mySQL connection](images/Connect_MYSQL.png)
+
+
+Product Display of all items available for sale. Includes id, name, department, price and quantity
+![Image of product display in the console](images/display_products.png)
+
+
+
+
+
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,69 +13,86 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     displayProducts();
-    purchase();
-})
+    
+});
 
 function displayProducts(){
     connection.query("SELECT * FROM products", function(err, res){
         if (err) throw err;
 
-        // console.log(res);
+        // console.log(res);logs all results
         for (var i = 0; i < res.length; i++){
-            console.log("Id: " + res[i].id +  " || Product: " + res[i].product_name + " || Price: " + res[i].price + "|| Quantity: " + res[i].stock_quantity);
+            console.log("Id: " + res[i].id +  " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "Quantity: " + res[i].stock_quantity);
            
            
         };
-        purchase(res);
-    })
+       purchase(res)
+    });
 };
 
-
-function purchase(){
+//creates two prompts, 1. asks the user to select the id of the product they want to buy; 2. asks how many of the product they would like to buy
+function purchase(inventory){
     inquirer.prompt([
         {
         type: "input",
-        name: "ID",
+        name: "item_id",
         message: "Select an id number of the product you would like to purchase" + ".\n",
-        // validate: function(value) {
-        //     if (isNaN(value) === false) {
-        //         return "true";
-        //     }
-        //     return false;
-        // },
+        validate: function(value) {
+            if (isNaN(value) === false) {
+                return true;
+            }
+            return false;
+        },
 
         },
         {
         type: "input",
         name: "quantity",
         message: "How many units would you like to purchase?",
-            // validate: function(value) {
-            //     if (isNaN(value) === false) {
-            //         return true;
-            //     }
-            //     return false;
-            // },
+        validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            },
         
         }
-    ]).then(function(res) {
-        // console.log(res);
-        console.log("checking inventory");
-        var id = res.id;
-        var quantity = res.quantity
-
-        // check if enough quantity exists to fulfill order
-        if (stock_quantity >= quantity) {
-            console.log("fill order")
-
+    ]).then(function(answer) {
+        console.log(answer);
+        // var chosenProduct;
+        var chosenItem;
+        for (var i = 0; i < inventory.length; i++) {
+            if (inventory[i].id === answer.item_id) {
+                chosenItem = answer.item_id;
+                console.log(chosenItem);
+            }
         }
-        //if not enough quantity exists, log insufficient quantity
-        else {
-            console.log("Insufficient quantity, unable to place order")
-        }
+      
+        
+            for (var i = 0; i < inventory.length; i++) {
+                if (inventory[i].id === parseInt(answer.item_id)) {
+                    chosenproduct = inventory[i];
+                    console.log(chosenProduct);
+                    // console.log(chosenProduct.quantity)
+                }
+            }; if (!chosenProduct) {
+                console.log("Please choose a valid product ID");
+            };
+    
+    
 
-        // if quantity available, fulfill the order
-        //update new quantity of item
-        //show customer the total cost of their purchase
+        // //use id.quantity to see if enough inventory exists
+        // if (chosenProduct){
+        //     if chosenProduct.stock_quantity >= parseInt(answer.quantity){
+        //         connection.query("UPDATE products SET stock_quantity = stock_quantity - "?"WHERE id = "?", [answer.quantity, chosenProduct.id], 
+        //         function (err) {
+        //             if (err) throw err;
+        //             console.log("Thank you for purchasing")
+        //         })
+        //     }
+        // }
+
+
     })
 
 }
